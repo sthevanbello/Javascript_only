2f1ac47ff07ae0e5819474e6ed4069f1a518482e
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-05-15 16:12:04 -0300

    First course of Javascript

---
+++ primeiro_programa.js
@@ -0,0 +1,19 @@
+// listas
+const cidades = new Array(`São Paulo`, `Rio De Janeiro`,`Salvador`,`Porto Alegre`,`Belo Horizonte`);
+
+// console.log(cidades);
+
+cidades.push(`Curitiba`);
+
+cidades.splice(3,1);
+
+let idade = 19;
+
+if (idade >= 18) {
+
+    console.log('Maior de idade');
+}else{
+    console.log("Menor de idade");
+}
+
+console.log(cidades);
